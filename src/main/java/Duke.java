f9f6879f79ea15b5e260d9dbd3effc4e8182b01e
darzizalol <darzizalol@gmail.com>
2025-08-16 21:51:26 +0800

    Level 2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -12,16 +13,26 @@ public class Duke {
 
         System.out.println(initMessage);
 
+        ArrayList<String> savedTexts = new ArrayList<>();
+
         // Detect user input
         while (sc.hasNextLine()) {
             String userInput = sc.nextLine();
             if (userInput.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (userInput.equals("list")) {
+                System.out.println("_________________________");
+                for (int i = 0; i < savedTexts.size(); i++) {
+                    System.out.println(String.valueOf(i + 1) + ". " + savedTexts.get(i));
+                }
+                System.out.println("_________________________");
+            } else {
+                System.out.println("_________________________");
+                System.out.println("added: " + userInput);
+                savedTexts.add(userInput);
+                System.out.println("_________________________");
             }
-            System.out.println("_________________________");
-            System.out.println(userInput);
-            System.out.println("_________________________");
         }
 
     }
