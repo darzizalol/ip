86d9b24c3066ab825789464c94991966050aae29
darzizalol <darzizalol@gmail.com>
2025-08-16 21:34:52 +0800

    Level 1: Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String message = "_________________________ \n" +
+
+        Scanner sc = new Scanner(System.in);
+
+        String initMessage = "_________________________ \n" +
                 "Hello! I'm ChatZH \n" +
                 "What can I do for you? \n" +
-                "_________________________\n" +
-                "Bye. Hope to see you again soon! \n" +
-                "_________________________";
-        System.out.println(message);
+                "_________________________ \n";
+
+        System.out.println(initMessage);
+
+        // Detect user input
+        while (sc.hasNextLine()) {
+            String userInput = sc.nextLine();
+            if (userInput.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println("_________________________");
+            System.out.println(userInput);
+            System.out.println("_________________________");
+        }
+
     }
 }
