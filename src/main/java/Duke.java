2cfdcde109ddcee22e506fbf4cafb4f2c5563328
darzizalol <darzizalol@gmail.com>
2025-08-16 21:24:35 +0800

    Level 0: Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,11 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String message = "_________________________ \n" +
+                "Hello! I'm ChatZH \n" +
+                "What can I do for you? \n" +
+                "_________________________\n" +
+                "Bye. Hope to see you again soon! \n" +
+                "_________________________";
+        System.out.println(message);
     }
 }
